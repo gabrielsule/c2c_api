40c146c6157f9dee1500cdf40ea8e9dd8f617360
Gabriel D. Sulé <gabrielsule@gmail.com>
2017-10-04 10:06:44 -0300

    beta

---
+++ api/car2city/Utils/JWTManager.cs
@@ -0,0 +1,115 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Security.Principal;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace car2city.Utils
+{
+    public class JWTManager
+    {
+        private const string Secret = "Q2hpbmdhVHVNYWRyZTY2Ng==";
+
+        public static string GenerateToken(string username, int expireMinutes = 20)
+        {
+            var symmetricKey = Convert.FromBase64String(Secret);
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            var now = DateTime.UtcNow;
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new[]
+                        {
+                        new Claim(ClaimTypes.Name, username)
+                    }),
+
+                Expires = now.AddMinutes(Convert.ToInt32(expireMinutes)),
+
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(symmetricKey), SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var stoken = tokenHandler.CreateToken(tokenDescriptor);
+            var token = tokenHandler.WriteToken(stoken);
+
+            return token;
+        }
+
+        public static ClaimsPrincipal GetPrincipal(string token)
+        {
+            try
+            {
+                var tokenHandler = new JwtSecurityTokenHandler();
+                var jwtToken = tokenHandler.ReadToken(token) as JwtSecurityToken;
+
+                if (jwtToken == null)
+                    return null;
+
+                var symmetricKey = Convert.FromBase64String(Secret);
+
+                var validationParameters = new TokenValidationParameters()
+                {
+                    RequireExpirationTime = true,
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                    IssuerSigningKey = new SymmetricSecurityKey(symmetricKey)
+                };
+
+                SecurityToken securityToken;
+                var principal = tokenHandler.ValidateToken(token, validationParameters, out securityToken);
+
+                return principal;
+            }
+
+            catch (Exception)
+            {
+               return null;
+            }
+        }
+
+        private static bool ValidateToken(string token, out string username)
+        {
+            username = null;
+
+            var simplePrinciple = GetPrincipal(token);
+            var identity = simplePrinciple.Identity as ClaimsIdentity;
+
+            if (identity == null)
+                return false;
+
+            if (!identity.IsAuthenticated)
+                return false;
+
+            var usernameClaim = identity.FindFirst(ClaimTypes.Name);
+            username = usernameClaim?.Value;
+
+            if (string.IsNullOrEmpty(username))
+                return false;
+
+            return true;
+        }
+
+        protected Task<IPrincipal> AuthenticateJwtToken(string token)
+        {
+            string username;
+
+            if (ValidateToken(token, out username))
+            {
+                var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, username)
+            };
+
+                var identity = new ClaimsIdentity(claims, "Jwt");
+                IPrincipal user = new ClaimsPrincipal(identity);
+
+                return Task.FromResult(user);
+            }
+
+            return Task.FromResult<IPrincipal>(null);
+        }
+    }
+}
